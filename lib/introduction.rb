6f08e0517f46b1b34213cb0e7aed4140c9020daa
Sierra Wallace <sskwallace@gmail.com>
2020-07-26 05:38:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction(name)
 end
 
 def introduction(name, language)
-  put "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
